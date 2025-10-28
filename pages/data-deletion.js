c78dbf9e8e1788eaeae38c62e4bc2563719597fa
uskpn <usk.okb.19413785@gmail.com>
2025-10-29 01:40:53 +0900

    Create data-deletion.js

---
+++ pages/data-deletion.js
@@ -0,0 +1,12 @@
+export default function DataDeletion() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Data Deletion Instructions</h1>
+      <p>
+        To delete your data, please contact us at{' '}
+        <a href="mailto:your@email.com">your@email.com</a>. 
+        We will remove your data from our system immediately.
+      </p>
+    </div>
+  );
+}
